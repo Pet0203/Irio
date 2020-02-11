821183d2d7f8ea4fe74e9b872c319ddf4f17b09a
Peter Mayer <020326pm@kristianstad.se>
2020-02-11 14:37:04 +0100

    Initial

---
+++ src/main/java/irio/engine/Irio.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package irio.engine;
+
+import java.awt.Canvas;
+
+/**
+ *
+ * @author 020326pm
+ */
+public class Irio extends Canvas {
+    private final static Irio instance = new Irio();
+    
+    //Entrypoint
+    public static void main(String[] args) {
+        instance.init();
+    }
+    
+    //Main game initiator
+    private void init() {
+        
+    }
+    
+    public static Irio getInstance(){
+        return instance;
+    }
+}
